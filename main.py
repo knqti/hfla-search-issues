a87196d4748276aafef3dc9fa784d8f1a030c27a
knqti <166448899+knqti@users.noreply.github.com>
2024-11-24 22:40:14 -0800

    Test getting repo's issues

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from ghapi.all import GhApi
 from token_file import GITHUB_TOKEN
 
-print(GITHUB_TOKEN)
+api = GhApi(token=GITHUB_TOKEN)
+
+issues = api.issues.list_for_repo(
+    owner='hackforla',
+    repo='knowledgebase-content',
+    labels='Complexity: Extra Large'
+)
+
+for issue in issues:
+    print(f"{issue.title} - {issue.html_url}")
